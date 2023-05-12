c7e064a0975217a378a96dae87f2040d6842986d
kristennss <kristenfernandez089@gmail.com>
2023-05-12 11:11:23 -0500

    Creamos la sentencia Switch-case para que cada dia tenga una variables

--- Ejercicio 1 js.js
+++ Ejercicio 1 js.js
@@ -2,6 +2,31 @@ var numerDia;
 const date=new Date();
 
 function getDay(numerDia=date.getDate()){
-    
+    let day;
+    switch(numerDia){
+        case 0:
+            day= "Domingo";
+            break;
+        case 1:
+            day="Lunes";
+            break;   
+        case 2:
+            day= "Martes";
+            break;
+        case 3:
+            day="Miercoles";
+            break;  
+        case 4:
+            day="Jueves";
+            break;   
+        case 5:
+            day= "Viernes";
+            break;
+        case 6:
+            day="Sabado";
+            break;         
+
+    }
+    console.log(day);
     
 }
